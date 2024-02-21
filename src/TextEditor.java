1dc3dcfb4ef99c33ec5b9ef8bd470db7718e9fee
Black-Smith-Of-Fire <blacksmithoffire@gmail.com>
2024-02-21 18:38:51 +0530

    Final modifications

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,8 +1,11 @@
 import java.awt.event.*;
+import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.util.Scanner;
 
 import static javax.swing.ScrollPaneConstants.*;
 
@@ -13,6 +16,12 @@ public class TextEditor extends JFrame implements ActionListener{
     JLabel fontLabel= new JLabel("Font : ");
     JButton fontColourButton = new JButton("Colour");
     JComboBox fontBox ;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
 
     TextEditor(){
         //Physical Properties
@@ -46,7 +55,24 @@ public class TextEditor extends JFrame implements ActionListener{
         fontBox.addActionListener(this);
         fontBox.setSelectedItem("Arial");
 
+        // menubar
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
 
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+
+        this.setJMenuBar(menuBar);
         this.add(fontSizeSpinner);
         this.add(fontColourButton);
         this.add(scrollPane);
@@ -62,6 +88,64 @@ public class TextEditor extends JFrame implements ActionListener{
 
                Color color = colorChooser.showDialog(null,"Choose a color",Color.BLACK);
                        textArea.setForeground(color);
+           }
+           if(e.getSource()==fontBox){
+            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));
+           }
+
+           if(e.getSource()==openItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files","txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()){
+                        while(fileIn.hasNextLine()){
+                            String line = fileIn.nextLine()+"/n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+                finally{
+                    fileIn.close();
+                }
+
+            }
+           }
+           
+           if(e.getSource()==saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response =fileChooser.showSaveDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut=null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try{
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                }catch(FileNotFoundException e1){
+                    e1.printStackTrace();
+                }
+                finally{
+                    fileOut.close();
+                }
+            }
+           }
+
+           if(e.getSource()==exitItem){
+            System.exit(0);
            }
     }
 }
